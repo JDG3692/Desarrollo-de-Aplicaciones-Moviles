344f21d12d436c5c6fad5db8c18eefb76fbc189d
JDG3692 <jonathan_36@hotmail.es>
2026-09-01 13:21:01 -0500

    Agrega estructura inicial de la pantalla de agenda

--- app/src/main/java/com/example/agendapersonal/MainActivity.kt
+++ app/src/main/java/com/example/agendapersonal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.agendapersonal.ui.theme.AgendaPersonalTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             AgendaPersonalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+    Column (modifier = modifier
+        .fillMaxSize()
+        .padding(24.dp))
+    {
+
+        Text("Agenda Personal")
+        Text("Organiza tu día")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AgendaPersonalTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
