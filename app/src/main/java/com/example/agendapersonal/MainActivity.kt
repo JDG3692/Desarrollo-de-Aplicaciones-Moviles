4425b1a26ec2379bb8a1003ee35d1f45a0afd928
JDG3692 <jonathan_36@hotmail.es>
2026-09-01 13:36:51 -0500

    Agrega botón para agregar tareas y manejo de estado

--- app/src/main/java/com/example/agendapersonal/MainActivity.kt
+++ app/src/main/java/com/example/agendapersonal/MainActivity.kt
@@ -7,12 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.agendapersonal.ui.theme.AgendaPersonalTheme
 
 
@@ -35,13 +42,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
-    Column (modifier = modifier
-        .fillMaxSize()
-        .padding(24.dp))
-    {
 
-        Text("Agenda Personal")
+    var tareaAgregada by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Text(
+            text = "Agenda Personal",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
+
         Text("Organiza tu día")
+
+        Button(
+            onClick = {
+                tareaAgregada = true
+            }
+        ) {
+            Text("Agregar tarea")
+        }
+        if (tareaAgregada){
+            Text("¡Tarea agregada!")
+        }
     }
 }
 
